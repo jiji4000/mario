dcc6c9062f543072f988b3c5cdef1e69b83aa6e4
jiji4000 <jiji4000alpha@gmail.com>
2018-02-15 14:21:54 +0900

    add chapter19 map chip collision with x align.

---
+++ chapter19(collision_with_mapchip_x)/const.js
@@ -0,0 +1,15 @@
+var LEFT_DIR = 0;
+var RIGHT_DIR = 1;
+// chapter10
+var MAX_MAP_X = 20;
+var MAX_MAP_Y = 15;
+// chapter11
+var DASH_SPEED = 5;
+var NORMAL_SPPED = 3;
+// chapter12
+var GRAVITY_POWER = 1;
+var MAX_GRAVITY = 8;
+// chapter18
+var MAP_SIZE = 32;
+// chapter19
+var HALF_MAP_SIZE = MAP_SIZE / 2;
