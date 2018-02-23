330c075a0f7dad476ea7baa15339c9e248ed0862
jiji4000 <jiji4000alpha@gmail.com>
2018-02-23 17:32:26 +0900

    add chapter20(マップチップとy軸方向の当たり判定

---
+++ chapter20(マップチップとy軸方向の当たり判定)/const.js
@@ -0,0 +1,13 @@
+var RIGHT_DIR = 1;
+var LEFT_DIR = 0;
+var MAX_MAP_X = 20;
+var MAX_MAP_Y = 15;
+
+var DASH_SPEED = 5;
+var NORMAL_SPPED = 3;
+// 重力値
+var GRAVITY_POWER = 1;
+// 最大の重力量
+var MAX_GRAVITY = 8;
+var MAP_SIZE = 32;
+var HALF_MAP_SIZE = 16;
