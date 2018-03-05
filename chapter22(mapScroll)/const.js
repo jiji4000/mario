c6d57631adb3f9cc780f1ec1d2db8d0a3a1779c1
jiji4000 <jiji4000alpha@gmail.com>
2018-03-05 19:06:25 +0900

    add chapter22 map scroll

---
+++ chapter22(mapScroll)/const.js
@@ -0,0 +1,18 @@
+var RIGHT_DIR = 1;
+var LEFT_DIR = 0;
+var MAX_MAP_X = 20;
+var MAX_MAP_Y = 15;
+
+var DASH_SPEED = 5;
+var NORMAL_SPPED = 3;
+// 重力値
+var GRAVITY_POWER = 1;
+// 最大の重力量
+var MAX_GRAVITY = 8;
+var MAP_SIZE = 32;
+var HALF_MAP_SIZE = 16;
+// Chapter22 Scroll
+// x座標のスクロール開始地点
+var SCROLL_POINT_X = (640 / 2) - (MAP_SIZE / 2);
+// マップチップの最大描画範囲(画面外+1のマップチップチップも描画させる)
+var DRAW_MAX_MAP_X = MAX_MAP_X + 1;
