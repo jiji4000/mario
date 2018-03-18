f1bab96a109d234f03b54d0ca5edb9ee2b24e4e1
jiji4000 <jiji4000alpha@gmail.com>
2018-03-18 10:55:19 +0900

    クリボとマップチップとの当たり判定を追加した

--- kuribo.js
+++ kuribo.js
@@ -8,6 +8,12 @@ function Kuribo(posX,posY,dir){
 	// 切り出す始点のX座標
 	this.animX = 0;
 	this.direction = dir;
+	// マップチップ座標
+	this.rightMapX = 0;
+	this.leftMapX = 0;
+	this.upMapY = 0;
+	this.downMapY = 0;
+
 }
 
 /*
@@ -26,10 +32,14 @@ Kuribo.prototype.draw = function(ctx,texture,scrollX){
 	moveNum:移動量
 */
 Kuribo.prototype.move = function(mapChip,moveNum){
+	this.updateMapPosition();
 	// 向きにより加算量を調整する
 	moveNum = this.direction == LEFT_DIR ? -moveNum : moveNum;
 	// 加算量を代入する
 	this.addPosX = moveNum;
+	// マップチップとの当たり判定
+	this.collisionX(mapChip,this.posX + this.addPosX);
+
 	this.posX += this.addPosX;
 
 	// animation
@@ -39,5 +49,85 @@ Kuribo.prototype.move = function(mapChip,moveNum){
 		if(++this.animX > 3){
 			this.animX = 0;
 		}
+	}
+}
+
+/**
+	x軸方向のマップチップ座標の更新
+
+	posX : マップチップ更新対象となるx座標
+*/
+Kuribo.prototype.updateMapPositionX = function(posX){
+	// x座標
+	this.leftMapX = Math.floor(posX / MAP_SIZE);
+	this.rightMapX = Math.floor((posX + MAP_SIZE - 1) / MAP_SIZE);
+
+	// 配列外チェック
+	if(this.leftMapX >= MAX_MAP_CHIP_X){
+		this.leftMapX = MAX_MAP_CHIP_X - 1;
+	}
+	if(this.leftMapX < 0){
+		this.leftMapX = 0;
+	}
+	if(this.rightMapX >= MAX_MAP_CHIP_X){
+		this.rightMapX = MAX_MAP_CHIP_X - 1;
+	}
+	if(this.rightMapX < 0){
+		this.rightMapX = 0;
+	}
+}
+
+/**
+	chapter26
+	Y軸方向のマップチップの更新
+*/
+Kuribo.prototype.updateMapPositionY = function(posY){
+	// y
+	this.upMapY = Math.floor(posY / MAP_SIZE);
+	this.downMapY = Math.floor((posY + MAP_SIZE - 1) / MAP_SIZE);
+
+	// 配列外チェック
+	if(this.upMapY >= MAX_MAP_Y - 1){
+		this.upMapY = MAX_MAP_Y - 1;
+	}
+	if(this.upMapY < 0){
+		this.upMapY = 0;
+	}
+	if(this.downMapY >= MAX_MAP_Y - 1){
+		this.downMapY = MAX_MAP_Y - 1;
+	}
+	if(this.downMapY < 0){
+		this.downMapY = 0;
+	}
+}
+
+/**
+	chapter26
+	マップチップ座標を更新する
+*/
+Kuribo.prototype.updateMapPosition = function(){
+	this.updateMapPositionX(this.posX);
+	this.updateMapPositionY(this.posY);
+}
+
+/**
+	chapter26
+	オブジェクトとの当たり判定X
+*/
+Kuribo.prototype.collisionX = function(map,posX){
+	this.updateMapPositionX(posX);
+	// クリボの右側
+	if(isObjectMap(map[this.downMapY][this.rightMapX]) || isObjectMap(map[this.upMapY][this.rightMapX])){
+		// (加算される前の)中心点からの距離を取る
+		var vecX = Math.abs((this.posX + HALF_MAP_SIZE) - ((this.rightMapX * MAP_SIZE) + HALF_MAP_SIZE));
+		this.addPosX = Math.abs(MAP_SIZE - vecX);
+		this.direction = LEFT_DIR;
+	}
+	// クリボの左側
+	else if(isObjectMap(map[this.downMapY][this.leftMapX]) || isObjectMap(map[this.upMapY][this.leftMapX])){
+		// (加算される前の)中心点からの距離を取る
+		var vecX = Math.abs((this.posX + HALF_MAP_SIZE) - ((this.leftMapX * MAP_SIZE) + HALF_MAP_SIZE));
+		this.addPosX = -Math.abs(MAP_SIZE - vecX);
+		this.direction = RIGHT_DIR;
 	}
 }
